0d84a041b43be654eb7a38748d0dff2a52821c0c
Weldev11 <wellingtonsantos110891@gmail.com>
2024-06-17 20:57:44 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function limparConsulta() {
-    document.getElementById('nomeConsulta').value = '';  // Limpa o campo de consulta
-    document.getElementById('listaFuncionarios').innerHTML = '';  // Limpa a lista de resultados
-}
